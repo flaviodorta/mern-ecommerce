91d134d06e20175af9e199646ab7c028f893f7f6
flaviodorta <dorta.dev@gmail.com>
2022-07-05 13:28:15 -0300

    feat: added auth router

---
+++ server/src/routes/auth.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { authController } from '../controllers/auth';
+import { loginCheck, isAuth, isAdmin } from '../middleware/auth';
+
+export const router = express.Router();
+
+router.post('/isadmin', authController.isAdmin);
+router.post('/signup', authController.signup);
+router.post('/signin', authController.signin);
+router.post('/user', loginCheck, isAuth, isAdmin, authController.allUsers);

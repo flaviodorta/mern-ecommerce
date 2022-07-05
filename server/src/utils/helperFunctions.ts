2d4d899095daf25a56f0718be4c0bb10517d02d8
flaviodorta <dorta.dev@gmail.com>
2022-07-05 00:02:47 -0300

    feat: added helper functions toTitleCase, validateEmail and emailCheckInDatabase

---
+++ server/src/utils/helperFunctions.ts
@@ -0,0 +1,27 @@
+import { userModel } from '../models/users';
+
+export const toTitleCase = (str: string) => {
+  return str.replace(/\w\S*/g, (txt: string) => {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLocaleLowerCase();
+  });
+};
+
+export const validateEmail = (mail: string) => {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const emailCheckInDatabase = async (email: string) => {
+  let userFound = await userModel
+    .findOne({ email: email })
+    .exec((err, data) => {
+      if (data) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+};

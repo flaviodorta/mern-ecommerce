15ece13bef3006f65b5fc19466ca90d90560c388
flaviodorta <dorta.dev@gmail.com>
2022-07-06 19:40:28 -0300

    refactor: changed models and controllers names to suffix Model and Controller

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -3,44 +3,53 @@ import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../config/keys';
 import { userModel } from '../models/user.model';
 
-export const loginCheck = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    let token = [...req.headers.token].join('') as string;
-    token = token.replace('Bearer', '');
-    let decode = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload;
-    req.userDetails = decode;
-    next();
-  } catch (err) {
-    res.json({
-      error: 'You must be logged in',
-    });
-  }
-};
+class AuthMiddleware {
+  private static instance: AuthMiddleware | null = null;
 
-export const isAuth = (req: Request, res: Response, next: NextFunction) => {
-  let { loggedInUserId } = req.body;
-  if (
-    !loggedInUserId ||
-    !req.userDetails._id ||
-    loggedInUserId !== req.userDetails._id
-  ) {
-    res.status(403).json({ error: 'You are not authenticate' });
+  public static getInstance(): AuthMiddleware {
+    if (!AuthMiddleware.instance) {
+      AuthMiddleware.instance = new AuthMiddleware();
+    }
+    return AuthMiddleware.instance;
   }
-  next();
-};
 
-export const isAdmin = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    let reqUser = await userModel.findById(req.body.loggedInUserId);
-    if (reqUser.userRole === 'user') {
-      res.status(403).json({ error: 'Access denied' });
+  loginCheck(req: Request, res: Response, next: NextFunction): void {
+    try {
+      let token = [...req.headers.token].join('') as string;
+      token = token.replace('Bearer', '');
+      let decode = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload;
+      req.userDetails = decode;
+      next();
+    } catch (err) {
+      res.json({
+        error: 'You must be logged in',
+      });
+    }
+  }
+
+  isAuth(req: Request, res: Response, next: NextFunction): void {
+    let { loggedInUserId } = req.body;
+    if (
+      !loggedInUserId ||
+      !req.userDetails._id ||
+      loggedInUserId !== req.userDetails._id
+    ) {
+      res.status(403).json({ error: 'You are not authenticate' });
     }
     next();
-  } catch {
-    res.status(404);
   }
-};
+
+  async isAdmin(req: Request, res: Response, next: NextFunction) {
+    try {
+      let reqUser = await userModel.findById(req.body.loggedInUserId);
+      if (reqUser.userRole === 'user') {
+        res.status(403).json({ error: 'Access denied' });
+      }
+      next();
+    } catch {
+      res.status(404);
+    }
+  }
+}
+
+export const authMiddleware = AuthMiddleware.getInstance();
--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { authController } from '../controllers/auth.controller';
-import { loginCheck, isAuth, isAdmin } from '../middleware/auth.middleware';
+import { authMiddleware } from '../middleware/auth.middleware';
 
 export const authRouter = express.Router();
 
 authRouter.post('/isadmin', authController.isAdmin);
 authRouter.post('/signup', authController.signup);
 authRouter.post('/signin', authController.signin);
-authRouter.post('/user', loginCheck, isAuth, isAdmin, authController.allUsers);
+authRouter.post(
+  '/user',
+  authMiddleware.loginCheck,
+  authMiddleware.isAuth,
+  authMiddleware.isAdmin,
+  authController.allUsers
+);
